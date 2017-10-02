40398642425344726a078ab71403364fd2d48836
Johnathan Chen <jc.swfit@icloud.com>
2017-10-01 17:03:16 -0700

    started adding more cells

--- moodTracker2/ListTableViewController.swift
+++ moodTracker2/ListTableViewController.swift
@@ -70,7 +70,6 @@ class ListTableViewController: UITableViewController, moodDelegate {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -80,7 +79,7 @@ class ListTableViewController: UITableViewController, moodDelegate {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
